752797843ec574318b0fe6be63cb1e0823c23437
Jeof Petty <jeofpetty@icloud.com>
2023-02-12 19:37:02 -0500

    testing

--- app.py
+++ app.py
@@ -1,12 +1,76 @@
 """Blogly application."""
 
-from flask import Flask
-from models import db, connect_db
+from flask import Flask, redirect, render_template, request
+from flask_debugtoolbar import DebugToolbarExtension
+from models import db, connect_db, User
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly'
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///blogly_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
+app.config['SECRET_KEY'] = 'mychickensteve'
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+debug = DebugToolbarExtension(app)
+
 connect_db(app)
+app.app_context().push()
 db.create_all()
+
+@app.route('/')
+def home_page():
+    return redirect('/users')
+
+@app.route('/users')
+def users():
+    users = User.query.all()
+    return render_template('users.html', users=users)
+
+@app.route('/users/new')
+def new_user_form():
+    return render_template('form.html')
+
+@app.route('/users/new', methods=['POST'])
+def post_new_user():
+    fname = request.form['first-name']
+    lname = request.form['last-name']
+    image = request.form['image-url']
+    if len(image) <= 0:
+        image = None
+
+    new_user = User(first_name=fname, last_name=lname, image_url=image)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect('/users')
+
+@app.route('/users/<user_id>')
+def profile(user_id):
+    user = User.query.get(user_id)
+    return render_template('profile.html', user=user)
+
+@app.route('/users/<user_id>/edit')
+def edit_profile(user_id):
+    user = User.query.get(user_id)
+    return render_template('edit_user.html', user=user)
+
+@app.route('/users/<user_id>/edit', methods=['POST'])
+def update_user(user_id):
+    user = User.query.get(user_id)
+    user.first_name = request.form['first-name']
+    user.last_name = request.form['last-name']
+    user.image_url = request.form['image-url']
+
+    db.session.add(user)
+    ad.session.commit()
+
+    return redirect('/users')
+
+@app.route('/users/<user_id>/delete', methods=['POST'])
+def delete_user(user_id):
+    user = User.query.get(user_id)
+
+    db.session.delete(user)
+    db.session.commit()
+    return redirect('/users')
--- models.py
+++ models.py
@@ -1,1 +1,16 @@
 """Models for Blogly."""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    first_name = db.Column(db.String(15), nullable=False)
+    last_name = db.Column(db.String(15))
+    image_url = db.Column(db.String, default='https://1fid.com/wp-content/uploads/2022/06/no-profile-picture-6-1024x1024.jpg')
